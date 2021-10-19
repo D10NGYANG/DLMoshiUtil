49195c1a317c11135fb342e5aec50ad61b0fe280
D10NG <D10NGYANG@163.com>
2021-10-19 16:11:21 +0800

    版本1.2(3) - 增加非空时使用默认值的factory；

--- MoshiLib/src/main/java/com/d10ng/moshilib/MoshiUtils.kt
+++ MoshiLib/src/main/java/com/d10ng/moshilib/MoshiUtils.kt
@@ -1,18 +1,25 @@
 package com.d10ng.moshilib
 
+import com.squareup.moshi.DefaultIfNullFactory
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import org.json.JSONObject
 import kotlin.Exception
 
 /** 获取Moshi构造器 */
-fun getMoshiBuilder() = Moshi.Builder().build()
+val moshi: Moshi by lazy {
+    Moshi.Builder()
+        .add(DefaultIfNullFactory())
+        .addLast(KotlinJsonAdapterFactory())
+        .build()
+}
 
 /**
  * 将Bean转换成Json
  * @receiver T
  * @return (kotlin.String..kotlin.String?)
  */
-inline fun <reified T> T.toJson() = getMoshiBuilder().adapter(T::class.java).toJson(this)
+inline fun <reified T> T.toJson() = moshi.adapter(T::class.java).toJson(this)
 
 /**
  * 将Bean转换成Json
@@ -20,7 +27,7 @@ inline fun <reified T> T.toJson() = getMoshiBuilder().adapter(T::class.java).toJ
  * @param clz Class<T>
  * @return (String..String?)
  */
-fun <T> T.toJson(clz: Class<T>) = getMoshiBuilder().adapter(clz).toJson(this)
+fun <T> T.toJson(clz: Class<T>) = moshi.adapter(clz).toJson(this)
 
 /**
  * 将object转换成实体
@@ -56,7 +63,7 @@ fun <T> Map<String, Any>.toMoshiBean(clz: Class<T>): T {
  * @return T
  */
 fun <T> JSONObject.toMoshiBean(clz: Class<T>): T {
-    val adapter = getMoshiBuilder().adapter(clz)
+    val adapter = moshi.adapter(clz)
     return try {
         adapter.fromJson(this.toString())?: clz.newInstance()
     } catch (e: Exception) {
